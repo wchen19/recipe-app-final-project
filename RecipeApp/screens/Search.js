1870d6c523bf147746d0702118433b39a08555c8
Wilbert Chen <wilbertchen2001@gmail.com>
2023-04-10 01:36:34 +0700

    fix illustration and add load more button

--- RecipeApp/screens/Search.js
+++ RecipeApp/screens/Search.js
@@ -12,12 +12,15 @@ import FastImage from 'react-native-fast-image';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {RecipeCard} from '../components';
 import {COLORS, SIZES, FONTS, getRecom, api} from '../constants';
+import {medium} from '@cloudinary/base/qualifiers/fontHinting';
 
 const Search = ({navigation}) => {
   const [recipes, setRecipes] = useState({});
   const [searchValue, setSearchValue] = useState('');
   const [showRecipes, setShowRecipes] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [recipeLimit, setRecipeLimit] = useState(10);
+  const [limitedRecipes, setLimitedRecipes] = useState([]);
   const inputRef = useRef(null);
 
   const fetchData = async ids => {
@@ -25,13 +28,23 @@ const Search = ({navigation}) => {
       .getRecipesByIds(ids)
       .then(response => {
         setRecipes(response.data);
+        const limitedRecipes = response.data.slice(0, 10);
+        setLimitedRecipes(limitedRecipes);
         setShowRecipes(true);
       })
       .catch(error => console.log(error));
   };
 
+  useEffect(() => {
+    if (recipes.length) {
+      const limitedRecipes = recipes.slice(0, recipeLimit);
+      setLimitedRecipes(limitedRecipes);
+    }
+  }, [recipeLimit]);
+
   const fetchRecipes = async ingredients => {
     setLoading(true);
+    setRecipeLimit(10);
     try {
       const result = await getRecom(ingredients);
       const str = JSON.stringify(Object.values(result)).replace(
@@ -105,6 +118,11 @@ const Search = ({navigation}) => {
     );
   };
 
+  const handleLoadMore = () => {
+    let limit = recipeLimit + 10;
+    setRecipeLimit(limit);
+  };
+
   return (
     <View
       style={{
@@ -112,11 +130,12 @@ const Search = ({navigation}) => {
         paddingTop: SIZES.radius,
         backgroundColor: COLORS.white,
         alignItems: 'center',
+        marginBottom: 70,
       }}>
       {renderSearchBar()}
       {showRecipes ? (
         <FlatList
-          data={recipes}
+          data={limitedRecipes}
           keyExtractor={item => `${item.id}`}
           keyboardDismissMode="on-drag"
           showsVerticalScrollIndicator={false}
@@ -135,7 +154,27 @@ const Search = ({navigation}) => {
               />
             );
           }}
-          ListFooterComponent={<View style={{marginBottom: 70}} />}
+          ListFooterComponent={
+            recipes.length > 10 &&
+            recipeLimit < recipes.length && (
+              <TouchableOpacity
+                style={{
+                  backgroundColor: COLORS.blue,
+                  padding: 10,
+                  alignItems: 'center',
+                  alignSelf: 'center',
+                  borderRadius: 10,
+                  marginVertical: 10,
+                  marginBottom: 10,
+                  width: '50%',
+                }}
+                onPress={handleLoadMore}>
+                <Text style={{color: COLORS.white, ...FONTS.h3}}>
+                  Load More
+                </Text>
+              </TouchableOpacity>
+            )
+          }
         />
       ) : loading ? (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -146,17 +185,19 @@ const Search = ({navigation}) => {
           <Text
             style={{
               color: COLORS.transparentBlack3,
+              fontWeight: 800,
               ...FONTS.h3,
               fontSize: 18,
             }}>
             Input ingredients seperated by comma
           </Text>
           <FastImage
-            source={require('../constants/images/Search.jpg')}
+            source={require('../constants/images/Search.png')}
+            resizeMode="center"
             style={{
-              width: 350,
-              height: 350,
-              opacity: 0.3,
+              width: 400,
+              height: 100,
+              opacity: 0.5,
               borderRadius: SIZES.radius,
             }}
           />
